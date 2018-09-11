0f1c419426234268a347013baf78bb64d0759543
Lia Meirose <liacmeirose@gmail.com>
2018-09-11 16:11:02 -0400

    This is my py file to average three numbers

--- average3.py
+++ average3.py
@@ -1,5 +1,5 @@
 def main():
-    work=add_three()
+    work=add_three(1,1,10)
     ans= divide_three(work)
     print('This is the output of divide_three:{}'.format(ans))
 
@@ -7,13 +7,18 @@ def add_three(v1, v2, v3):
     """Adds three numbers together
     :param v1: number one
     :param v2: number two
-    :param v3: number three
+    :param v3: number three"""
     a= v1 + v2 + v3
     print(a)
     return a
 
-def divide_three(input)
-    """Divides input by 3
+
+def divide_three(input):
+    """Divides input by 3"""
     soln=input/3
     print(soln)
+    return soln
+
+if __name__== "__main__":
+    main()    
     
